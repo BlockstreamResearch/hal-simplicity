fa80dbbc1b97e7d5e328048ad3d4464d9dd2688f
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-10-25 14:49:35 +0000

    simplicity: add `simplicity pset update-input` CLI call

--- src/bin/hal-simplicity/cmd/simplicity/mod.rs
+++ src/bin/hal-simplicity/cmd/simplicity/mod.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: CC0-1.0
 
 mod info;
+mod pset;
 mod sighash;
 
 use crate::cmd;
@@ -34,12 +35,14 @@ impl<T, E: core::fmt::Display> ErrorExt<T> for Result<T, E> {
 pub fn subcommand<'a>() -> clap::App<'a, 'a> {
 	cmd::subcommand_group("simplicity", "manipulate Simplicity programs")
 		.subcommand(self::info::cmd())
+		.subcommand(self::pset::cmd())
 		.subcommand(self::sighash::cmd())
 }
 
 pub fn execute<'a>(matches: &clap::ArgMatches<'a>) {
 	match matches.subcommand() {
 		("info", Some(m)) => self::info::exec(m),
+		("pset", Some(m)) => self::pset::exec(m),
 		("sighash", Some(m)) => self::sighash::exec(m),
 		(_, _) => unreachable!("clap prints help"),
 	};
