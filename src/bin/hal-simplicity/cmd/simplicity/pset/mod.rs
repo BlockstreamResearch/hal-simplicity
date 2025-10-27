f60d0a0394755126d68cb3be8e5c65485384aa0c
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-10-27 20:04:16 +0000

    simplicity: add 'simplicity pset finalize' command
    This command takes a PSET where we've updated all the inputs (with
    'simplicity pset update-input' for example), as well as a program and
    a valid witness. It executes the program, prunes it, then adds it to
    the PSET. Then you can extract and send the program with elements-cli.

--- src/bin/hal-simplicity/cmd/simplicity/pset/mod.rs
+++ src/bin/hal-simplicity/cmd/simplicity/pset/mod.rs
@@ -1,17 +1,27 @@
 // Copyright 2025 Andrew Poelstra
 // SPDX-License-Identifier: CC0-1.0
 
+mod finalize;
 mod update_input;
 
 use crate::cmd;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct UpdatedPset {
+	pset: String,
+	updated_values: Vec<&'static str>,
+}
 
 pub fn cmd<'a>() -> clap::App<'a, 'a> {
 	cmd::subcommand_group("pset", "manipulate PSETs for spending from Simplicity programs")
+		.subcommand(self::finalize::cmd())
 		.subcommand(self::update_input::cmd())
 }
 
 pub fn exec<'a>(matches: &clap::ArgMatches<'a>) {
 	match matches.subcommand() {
+		("finalize", Some(m)) => self::finalize::exec(m),
 		("update-input", Some(m)) => self::update_input::exec(m),
 		(_, _) => unreachable!("clap prints help"),
 	};
