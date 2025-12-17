4ba74c325f83f3f070b669b92538d85f433fa386
ivanlele <ivanlele402@gmail.com>
2025-12-17 17:45:47 +0200

    Move the hal simplicity daemon under the daemon feature

--- hal-simplicity-daemon/src/lib.rs
+++ hal-simplicity-daemon/src/lib.rs
@@ -1,9 +1,13 @@
 pub extern crate simplicity;
 
-pub mod daemon;
-pub mod jsonrpc;
-pub mod utils;
-
 pub mod types;
 
+#[cfg(feature = "daemon")]
+pub mod daemon;
+#[cfg(feature = "daemon")]
+pub mod jsonrpc;
+#[cfg(feature = "daemon")]
+pub mod utils;
+
+#[cfg(feature = "daemon")]
 pub use daemon::HalSimplicityDaemon;
